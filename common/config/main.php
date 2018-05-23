dcfe040d44e3dc8a47965653eae32b7d53aff50c
e <armshan@163.com>
2018-05-23 18:06:58 +0800

    美化路由 配置位置改变至公共文件

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'suffix' => '.html',
+            'rules' => [
+                'posts' => 'post/index',
+                'post/<id:\d+>' => 'post/view',
+                '<controller:(post|comment)>/<id:\d+>/<action:(create|update|delete)>' => '<controller>/<action>',
+                'DELETE <controller:\w+>/<id:\d+>' => '<controller>/delete',
+                'http://<user:\w+>.digpage.com/<lang:\w+>/profile' => 'user/profile',
+            ],
+        ],
     ],
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,18 +36,6 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'suffix' => '.html',
-            'rules' => [
-                'posts' => 'post/index',
-                'post/<id:\d+>' => 'post/view',
-                '<controller:(post|comment)>/<id:\d+>/<action:(create|update|delete)>' => '<controller>/<action>',
-                'DELETE <controller:\w+>/<id:\d+>' => '<controller>/delete',
-                'http://<user:\w+>.digpage.com/<lang:\w+>/profile' => 'user/profile',
-            ],
-        ],
     ],
     'params' => $params,
 ];
